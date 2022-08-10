30482529406952af011aac426773925731cb7838
aleksei7gnevashev@gmail.com <aleksei7gnevashev@gmail.com>
2022-08-10 23:59:19 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,134 @@
+import datetime as dt
+
+
+class Record:
+    def __init__(self, amount, comment, date=''):
+        self.amount = amount
+        self.date = (
+            dt.datetime.now().date() if
+            not
+            date else dt.datetime.strptime(date, '%d.%m.%Y').date())
+        self.comment = comment
+
+
+class Calculator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+
+    def add_record(self, record):
+        # Нужно прописать документацию для этого метода, чтобы было удобнее работать.
+        # Удобнее как другим разработчикам, так и тебе.
+        # Для создания шаблона документации вставь """ в следующей строке и нажми Enter.
+        self.records.append(record)
+
+    def get_today_stats(self):
+        # Нужно прописать документацию для этого метода, чтобы было удобнее работать.
+        # Удобнее как другим разработчикам, так и тебе.
+        # Для создания шаблона документации вставь """ в следующей строке и нажми Enter.
+        today_stats = 0
+        for Record in self.records:
+            # в наименовании переменной Record лучше не использовать uppercase, лучше только lowercase
+            # чтобы не путать например с названием класса
+            if Record.date == dt.datetime.now().date():
+                today_stats = today_stats + Record.amount
+        return today_stats
+
+    def get_week_stats(self):
+        # Нужно прописать документацию для этого метода, чтобы было удобнее работать.
+        # Удобнее как другим разработчикам, так и тебе.
+        # Для создания шаблона документации вставь """ в следующей строке и нажми Enter.
+        week_stats = 0
+        today = dt.datetime.now().date()
+        for record in self.records:
+            if (
+                (today - record.date).days < 7 and
+                (today - record.date).days >= 0
+                # Это условие можно написать более коротко: 7 > (today - record.date).days >= 0
+            ):
+                week_stats += record.amount
+        return week_stats
+
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):  # Получает остаток калорий на сегодня
+        # Нужно прописать документацию для этого метода, чтобы было удобнее работать.
+        # Удобнее как другим разработчикам, так и тебе.
+        # Для создания шаблона документации вставь """ в следующей строке и нажми Enter.
+        x = self.limit - self.get_today_stats()
+        if x > 0:
+            return f'Сегодня можно съесть что-нибудь' \
+                   f' ещё, но с общей калорийностью не более {x} кКал'
+            # Внутри первой строки нет переменных, поэтому f-строка не нужна
+            # Не нужно делать перенос бэкслешем лучше возвращать значения строк другим методом
+            # например присвоение этих строк в переменные и объединять эти переменные в return с помощью +
+        else:
+            return('Хватит есть!') # Здесь не требуются () и нужен пробел между return и его объектом
+
+
+class CashCalculator(Calculator):
+    USD_RATE = float(60)  # Курс доллар США.
+    EURO_RATE = float(70)  # Курс Евро.
+    # Переменные нельзя называть в uppercase + их можно было объявить внутри метода get_today_cash_remained
+    # поскольку они используются только там, и не пришлось бы присваивать их на входных параметрах
+
+    def get_today_cash_remained(self, currency,
+                                USD_RATE=USD_RATE, EURO_RATE=EURO_RATE):
+        # Аргументы функции тоже должны быть в lowercase
+        # Нужно прописать документацию для этого метода, чтобы было удобнее работать.
+        # Удобнее как другим разработчикам, так и тебе.
+        # Для создания шаблона документации вставь """ в следующей строке и нажми Enter.
+        currency_type = currency
+        cash_remained = self.limit - self.get_today_stats()
+        if currency == 'usd':
+            cash_remained /= USD_RATE
+            currency_type = 'USD'
+        elif currency_type == 'eur':
+            cash_remained /= EURO_RATE
+            currency_type = 'Euro'
+        elif currency_type == 'rub':
+            cash_remained == 1.00
+            # строка 90 не требуется, по сути она бесполезна для подобного кейса лучше использовать функцию float()
+            currency_type = 'руб'
+        if cash_remained > 0:
+            return (
+                f'На сегодня осталось {round(cash_remained, 2)} '
+                f'{currency_type}'
+            )
+        # Нужно придерживаться единого стандарта в return подобно тому который реализован выше
+        # из всех он самый корректный
+        elif cash_remained == 0:
+            return 'Денег нет, держись'
+        elif cash_remained < 0:
+            return 'Денег нет, держись:' \
+                   ' твой долг - {0:.2f} {1}'.format(-cash_remained,
+                                                     currency_type)
+        # Не нужно делать перенос бэкслешем лучше возвращать значения строк другим методом
+        # например присвоение этих строк в переменные и объединять эти переменные в return
+
+    def get_week_stats(self):
+        # Нужно прописать документацию для этого метода, чтобы было удобнее работать.
+        # Удобнее как другим разработчикам, так и тебе.
+        # Для создания шаблона документации вставь """ в следующей строке и нажми Enter.
+        super().get_week_stats()
+
+
+# создадим калькулятор денег с дневным лимитом 1000
+cash_calculator = CashCalculator(1000)
+calories_calculator = CaloriesCalculator(400)
+# дата в параметрах не указана,
+# так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
+cash_calculator.add_record(Record(amount=145, comment="кофе"))
+calories_calculator.add_record(Record(amount=70, comment="кофе"))
+# и к этой записи тоже дата должна добавиться автоматически
+cash_calculator.add_record(Record(amount=300.25, comment="Серёге за обед"))
+calories_calculator.add_record(Record(amount=200, comment="обед с Серёгай"))
+# а тут пользователь указал дату, сохраняем её
+cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
+cash_calculator.add_record(Record(amount=1000, comment="бар в Танин др", date="08.11.2019"))
+
+print(cash_calculator.get_today_cash_remained("rub"))
+print(calories_calculator.get_calories_remained())
+# должно напечататься
+# На сегодня осталось 555 руб
+
